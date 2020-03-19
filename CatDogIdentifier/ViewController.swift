c0e0f670be89b63436c66f1d411638f741e0493f
konradgnat <konradmgnat@gmail.com>
2020-03-18 21:14:37 -0500

    Initial Commit

---
+++ CatDogIdentifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatDogIdentifier
+//
+//  Created by Konrad Gnat on 3/18/20.
+//  Copyright © 2020 Konrad Gnat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
