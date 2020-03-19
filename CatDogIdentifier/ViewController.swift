a0641d0ac27179b39dc5a5aef2d6df7644e1ab0b
konradgnat <konradmgnat@gmail.com>
2020-03-18 22:10:44 -0500

    complete app

--- CatDogIdentifier/ViewController.swift
+++ CatDogIdentifier/ViewController.swift
@@ -9,12 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var predictionLabel: UILabel!
+    
+    private var images: [String] = [String]()
+    
+    private var currentIndex: Int = 0
+    
+    let model = CatsAndDogs();
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadImages()
+
+        if let imageURL = images.first {
+            displayImage(imageURL: imageURL)
+        }
+        print(self.images.isEmpty)
+        if self.images.isEmpty {
+            if let imageURL = images.first {
+                displayImage(imageURL: imageURL)
+            }
+        }
+    }
+    
+    private func displayImage(imageURL: String) {
+        
+        let img = UIImage(named: imageURL)
+        self.photoImageView.image = img
+        
+        showPredictions(img)
+        
+    }
+    
+    private func showPredictions(_ img: UIImage?) {
+        
+        guard let img = img,
+            let resizedImage = img.resizeTo(size: CGSize(width: 299, height: 299)),
+            let pixelBuffer = resizedImage.pixelBuffer()
+            else {
+                fatalError("Incompatible image")
+        }
+        
+        if let prediciton = try? model.prediction(image: pixelBuffer) {
+            self.predictionLabel.text = prediciton.classLabel
+        }
     }
 
+    private func loadImages() {
+        
+        self.images = Bundle.main.paths(forResourcesOfType: "png", inDirectory: "test_images")
+        
+    }
+    
+    @IBAction func nextButtonPressed() {
+        print(self.images.count)
+        print(self.currentIndex)
+        
+        if self.currentIndex <= self.images.count - 1 {
+            displayImage(imageURL: self.images[self.currentIndex])
+            self.currentIndex += 1
+        } else {
+            self.currentIndex = 0
+        }
+        
+    }
 
 }
 
